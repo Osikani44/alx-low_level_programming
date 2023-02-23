9c5236b1dfbd223d1f21d4dba23cc84443ff48c6
nana <nanaakuffo44@gmail.com>
2023-02-23 12:34:49 -0800

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print diagonal - Draws a diagonal line
+ * @n: integer
+ */
+
+void print_diagonal(int n)
+{
+	int j, k;
+
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (j = 1; j <= n; j++)
+	{
+	for (k = 1; k < j; j++)
+	{
+	_putchar(' ');
+	}
+	_putchar('\\')
+	_putchar('\n')
+	}
+	}
+}
