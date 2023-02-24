684b943d8263554044773a76a3e42b7259b4d7e7
nana <nanaakuffo44@gmail.com>
2023-02-24 12:09:13 -0800

    alpha10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,14 +3,14 @@
 /**
  * print_alphabet_x10 - prints the alphabet 10 times.
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int print_alphabet_x10(void)
 {
 	int i;
 	int j;
 
-	for (j = i; <= 10; i++)
+	for (j = 1; <= 10; i++)
 	{
 		for (j = 97; j <= 122; j++)
 		{
