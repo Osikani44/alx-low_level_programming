596f5ac836f2929e92b969e4f4d592407a9140f5
nana <nanaakuffo44@gmail.com>
2023-02-23 21:59:30 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,14 +2,14 @@
 #include "main.h"
 
 /**
- * main - Prints a Fizz
+  * main - Prints a fizz buzz program
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	int x;
-	
+
 	for (x = 1; x <= 100; x++)
 	{
 	if ((x % 3 == 0) && (x % 5 == 0))
