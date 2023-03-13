c19848682c285d388347c44b2a2ccbbd4630af81
nana <nanaakuffo44@gmail.com>
2023-03-13 12:48:04 -0700

    final

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -37,12 +37,16 @@ int count;
 int str_to_int;
 int sum = 0;
 count = 1;
-while (count < argc) /*Goes through the whole array*/
+while (count < argc)
+/*
+ * Goes through the whole array
+ */
 {
 if (check_num(argv[count]))
 {
-str_to_int = atoi(argv[count]); /*ATOI --> convert string to
-int*/
+str_to_int = atoi(argv[count]);
+/*ATOI --> convert string to int
+ */
 sum += str_to_int;
 }
 /*Condition if one of the number contains symbols that are not
